0e3af0514dec0d4399211d1bb524ea39444a541c
William Wei <php3@carpetcall.com.au>
2022-08-19 17:12:16 +1000

    The new version of Code 117

--- Spring/OrdersService/src/main/java/com/appsdeveloperblog/estore/OrdersService/query/OrderEventsHandler.java
+++ Spring/OrdersService/src/main/java/com/appsdeveloperblog/estore/OrdersService/query/OrderEventsHandler.java
@@ -8,6 +8,7 @@ package com.appsdeveloperblog.estore.OrdersService.query;
 import com.appsdeveloperblog.estore.OrdersService.core.data.OrderEntity;
 import com.appsdeveloperblog.estore.OrdersService.core.data.OrdersRepository;
 import com.appsdeveloperblog.estore.OrdersService.core.events.OrderCreatedEvent;
+
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
